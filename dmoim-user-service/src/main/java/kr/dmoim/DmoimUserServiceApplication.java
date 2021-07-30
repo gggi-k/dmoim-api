e58de3ea8812d285eadd22d0a537aaa7ef4bc466
jprgr <jprgr16@gmail.com>
2021-07-30 19:27:57 +0900

    setting

---
+++ dmoim-user-service/src/main/java/kr/dmoim/DmoimUserServiceApplication.java
@@ -0,0 +1,12 @@
+package kr.dmoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmoimUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmoimUserServiceApplication.class, args);
+    }
+}
